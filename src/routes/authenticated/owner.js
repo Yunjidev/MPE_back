3da6d916a2f3b8d72d22145719123a10dd39c372
WinnyDrancourt <winny.morel@orange.fr>
2024-08-05 15:14:08 +0200

    Fix des routes

--- src/routes/authenticated/owner.js
+++ src/routes/authenticated/owner.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 // Middlewares
 const { upload } = require("../../middlewares/files-middleware");
+const fileMiddleware = require("../../middlewares/files-middleware");
 // Controllers
 const authController = require("../../controllers/auth-controller");
 const enterprisesController = require("../../controllers/enterprises-controller");
@@ -11,10 +12,16 @@ const inDisponibilitiesController = require("../../controllers/indisponibility-c
 const offersController = require("../../controllers/offer-controller");
 
 // Route Enterprise
+router.put(
+  "/",
+  fileMiddleware.upload("enterprise-photos").array("photos", 5),
+  enterprisesController.updateEnterprise,
+);
 router.delete("", enterprisesController.deleteEnterprise);
 
 // Routes Disponibilité
 router.post("/disponibility", disponibilitiesController.createDisponibility);
+router.use("/disponibility/:id", disponibilitiesController.updateDisponibility);
 router.delete(
   "/disponibility/:id",
   disponibilitiesController.deleteDisponibility,
@@ -24,6 +31,10 @@ router.delete(
 router.post(
   "/indisponibility",
   inDisponibilitiesController.createInDisponibility,
+);
+router.use(
+  "/indisponibility/:id",
+  inDisponibilitiesController.updateInDisponibility,
 );
 router.delete(
   "/indisponibility/:id",
@@ -36,10 +47,16 @@ router.post(
   upload("offer-image").single("image"),
   offersController.createOffer,
 );
+router.use(
+  "/offer/:id",
+  fileMiddleware.upload("offer-image").single("image"),
+  offersController.updateOffer,
+);
 router.delete("/offer/:id", offersController.deleteOffer);
 
 // Routes Subscription
 router.post("/subscription", subscriptionsController.createSubscription);
+router.use("/subscription/:id", subscriptionsController.updateSubscription);
 router.delete("/subscription/:id", subscriptionsController.deleteSubscription);
 
 module.exports = router;
