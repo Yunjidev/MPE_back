ae5d8367299c01f053dc8b2d32f8b81908d778f3
WinnyDrancourt <winny.morel@orange.fr>
2024-08-09 07:24:38 +0200

    Route User profile
    une route

--- src/controllers/users/user-controller.js
+++ src/controllers/users/user-controller.js
@@ -140,3 +140,106 @@ exports.getUserById = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.user.id, {
+      attributes: {
+        exclude: [
+          "password",
+          "resetPasswordToken",
+          "resetPasswordExpires",
+          "createdAt",
+          "updatedAt",
+        ],
+      },
+      include: [
+        {
+          model: sequelize.models.Enterprise,
+          as: "enterprises",
+          attributes: {
+            exclude: [
+              "createdAt",
+              "updatedAt",
+              "User_id",
+              "Job_id",
+              "Country_id",
+            ],
+          },
+        },
+        {
+          model: sequelize.models.Reservation,
+          as: "reservations",
+          attributes: {
+            exclude: ["createdAt", "updatedAt"],
+          },
+          include: {
+            model: sequelize.models.Offer,
+            as: "offer",
+            attributes: {
+              exclude: ["createdAt", "updatedAt"],
+            },
+            include: {
+              model: sequelize.models.Enterprise,
+              as: "enterprise",
+              attributes: {
+                exclude: [
+                  "createdAt",
+                  "updatedAt",
+                  "id",
+                  "User_id",
+                  "Job_id",
+                  "Country_id",
+                  "isValidate",
+                  "facebook",
+                  "instagram",
+                  "twitter",
+                  "siret_number",
+                  "description",
+                  "website",
+                  "photos",
+                ],
+              },
+            },
+          },
+        },
+        {
+          model: sequelize.models.Like,
+          as: "likes",
+          attributes: {
+            exclude: ["createdAt", "updatedAt", "User_id", "Enterprise_id"],
+          },
+          include: {
+            model: sequelize.models.Enterprise,
+            as: "enterprise",
+            attributes: {
+              exclude: [
+                "createdAt",
+                "updatedAt",
+                "id",
+                "User_id",
+                "Job_id",
+                "Country_id",
+                "isValidate",
+                "facebook",
+                "instagram",
+                "twitter",
+                "siret_number",
+                "description",
+                "website",
+                "photos",
+                "adress",
+              ],
+            },
+          },
+        },
+      ],
+    });
+    if (!user) {
+      return res.status(404).json({ message: "Pas d'utilisateur trouvé" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
