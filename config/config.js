7d67db38170b581bbf5e0c27c18fafbb7b2ea350
WinnyDrancourt <winny.morel@orange.fr>
2024-08-22 07:25:03 +0200

    Config
    config pre prod

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: "postgres",
+    password: "viny",
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+};
--- models/index.js
+++ models/index.js
@@ -6,19 +6,20 @@ const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
+const config = require("../config/config")[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config,
-  );
+  sequelize = new Sequelize({
+    database: config.database,
+    username: config.username,
+    password: config.password,
+    host: config.host,
+    dialect: config.dialect,
+  });
 }
 
 fs.readdirSync(__dirname)
