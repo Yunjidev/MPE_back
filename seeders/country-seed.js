092415794156adb71fdd1bdb37631a14aea00c6c
Milhan Verschelle <m.verschelle@ivsoftware.fr>
2025-08-11 11:55:49 +0200

    Ajout d'un fichier de seed pour les pays avec des données initiales

---
+++ seeders/country-seed.js
@@ -0,0 +1,84 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const hash = await bcrypt.hash('admin123', 10);
+    await queryInterface.bulkInsert(
+      'Countries',
+      [
+        {
+          name: "Provence-Alpes-Côte d'Azur",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Pays de la Loire',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Occitanie',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Nouvelle-Aquitaine',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Normandie',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Île-de-France',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Hauts-de-France',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Grand Est',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Corse',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Centre-Val de Loire',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Bretagne',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Bourgogne-Franche-Comté',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: 'Auvergne-Rhône-Alpes',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Countries');
+  },
+};
